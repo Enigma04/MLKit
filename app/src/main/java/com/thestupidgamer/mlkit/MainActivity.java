1cd074b640223ee764217d011ee04c6bf0cb34b1
APPLE <apple@APPLEs-MacBook-Pro.local>
2019-03-05 20:33:51 +0530

    Initial commit

---
+++ app/src/main/java/com/thestupidgamer/mlkit/MainActivity.java
@@ -0,0 +1,211 @@
+package com.thestupidgamer.mlkit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.nfc.Tag;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
+import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.RequiresApi;
+
+public class MainActivity extends AppCompatActivity {
+    //declaring references
+    ImageView imageView;
+    Button snap_button;
+    Button detect_button;
+    TextView result;
+    Bitmap imageBitmap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //referencing the layouts with variables
+        imageView = findViewById(R.id.imgview);
+        snap_button = findViewById(R.id.snap);
+        detect_button = findViewById(R.id.detect);
+        result = findViewById(R.id.result);
+
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    //method to open camera using intent
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+        }
+    }
+    //method to convert the pic into bitmap and displaying it on screen
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+             imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    //onclick (snap button)
+    public void click(View view)
+    {
+        dispatchTakePictureIntent();
+    }
+    //onclick (detect button)
+    public void detect(View view)
+    {
+        detectimg();
+    }
+
+    //process for detecting text from img
+    public void detectimg()
+    {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getCloudTextRecognizer();
+        FirebaseVisionCloudTextRecognizerOptions options = new FirebaseVisionCloudTextRecognizerOptions.Builder()
+                .setLanguageHints(Arrays.asList("en", "hi"))
+                .build();
+        Task<FirebaseVisionText> result =
+                detector.processImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                // Task completed successfully
+                                // ...
+                                processText(firebaseVisionText);
+                                //experiment(firebaseVisionDocumentText);
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                        Log.d("mlkit","Error");
+                                    }
+                                });
+
+
+
+    }
+    public void processText(FirebaseVisionText text)
+    {
+        for(FirebaseVisionText.TextBlock block: text.getTextBlocks()) {
+            String txt = block.getText();
+            result.setTextSize(24);
+            result.setText(txt);
+        }
+    }
+
+    /*public void experiment(FirebaseVisionDocumentText text1)
+    {
+        for (FirebaseVisionDocumentText.Block block: text1.getBlocks()) {
+            String blockText = block.getText();
+            Float blockConfidence = block.getConfidence();
+            List<RecognizedLanguage> blockRecognizedLanguages = block.getRecognizedLanguages();
+            Rect blockFrame = block.getBoundingBox();
+            for (FirebaseVisionDocumentText.Paragraph paragraph: block.getParagraphs()) {
+                String paragraphText = paragraph.getText();
+                Float paragraphConfidence = paragraph.getConfidence();
+                List<RecognizedLanguage> paragraphRecognizedLanguages = paragraph.getRecognizedLanguages();
+                Rect paragraphFrame = paragraph.getBoundingBox();
+                for (FirebaseVisionDocumentText.Word word: paragraph.getWords()) {
+                    String wordText = word.getText();
+                    Float wordConfidence = word.getConfidence();
+                    List<RecognizedLanguage> wordRecognizedLanguages = word.getRecognizedLanguages();
+                    Rect wordFrame = word.getBoundingBox();
+                    for (FirebaseVisionDocumentText.Symbol symbol: word.getSymbols()) {
+                        String symbolText = symbol.getText();
+                        Float symbolConfidence = symbol.getConfidence();
+                        List<RecognizedLanguage> symbolRecognizedLanguages = symbol.getRecognizedLanguages();
+                        Rect symbolFrame = symbol.getBoundingBox();
+                    }
+                }
+            }
+        }
+    }*/
+    //this enables firebase to detect texts from any angle
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
+    /**
+     * Get the angle by which an image must be rotated given the device's current
+     * orientation.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private int getRotationCompensation(String cameraId, Activity activity, Context context)
+            throws CameraAccessException {
+        // Get the device's current rotation relative to its "native" orientation.
+        // Then, from the ORIENTATIONS table, look up the angle the image must be
+        // rotated to compensate for the device's rotation.
+        int deviceRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int rotationCompensation = ORIENTATIONS.get(deviceRotation);
+
+        // On most devices, the sensor orientation is 90 degrees, but for some
+        // devices it is 270 degrees. For devices with a sensor orientation of
+        // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
+        CameraManager cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
+        int sensorOrientation = cameraManager
+                .getCameraCharacteristics(cameraId)
+                .get(CameraCharacteristics.SENSOR_ORIENTATION);
+        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
+
+        // Return the corresponding FirebaseVisionImageMetadata rotation value.
+        int result;
+        switch (rotationCompensation) {
+            case 0:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+                break;
+            case 90:
+                result = FirebaseVisionImageMetadata.ROTATION_90;
+                break;
+            case 180:
+                result = FirebaseVisionImageMetadata.ROTATION_180;
+                break;
+            case 270:
+                result = FirebaseVisionImageMetadata.ROTATION_270;
+                break;
+            default:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+        }
+        return result;
+    }
+}
